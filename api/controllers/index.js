3446cce13e8364598929733d0c5b946988ffba90
Ertugrul Alex Haskan <ertughaskan@gmail.com>
2022-01-20 00:33:15 -0800

    deleting a post

--- api/controllers/index.js
+++ api/controllers/index.js
@@ -49,3 +49,16 @@ export const updatePost = (req, res) => {
     }
   );
 };
+
+// Delete an article
+export const deletePost = (req, res) => {
+  Post.remove({ _id: req.params.id }, (err, blogPost) => {
+    if (err) {
+      res.send(err);
+    }
+    res.json({
+      status: true,
+      message: "The post succesfully removed!",
+    });
+  });
+};
--- api/routes/index.js
+++ api/routes/index.js
@@ -3,25 +3,26 @@ import {
   getAllPosts,
   getPostWithID,
   updatePost,
+  deletePost,
 } from "../controllers";
 
 const routes = (app) => {
+  // Getting all posts and Adding a new post
   app
     .route("/api/article")
     .get((req, res, next) => {
       // Middleware
-      console.info(`Captain's Log \n ${req}`);
+      // console.info(`Captain's Log \n ${req}`);
       next();
     }, getAllPosts)
     .post(newBlogPost);
 
+  // Post read, update, and delete
   app
     .route("/api/article/:id")
     .get(getPostWithID)
     .put(updatePost)
-    .delete((res, req) => {
-      res.send("DELETE Method");
-    });
+    .delete(deletePost);
 };
 
 export default routes;
